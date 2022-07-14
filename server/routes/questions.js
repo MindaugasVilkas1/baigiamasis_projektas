cf2ef3b86dd2306756f8c54715d7616b4b164842
MindaugasVilkas1 <mindaugas.vilkas@codeacademylt.onmicrosoft.com>
2022-07-14 12:54:01 +0300

    Quosestion post istaisytos validacjos problemos

--- server/routes/questions.js
+++ server/routes/questions.js
@@ -1,6 +1,6 @@
 import express from "express";
 import fetch from 'node-fetch';
-
+import axios from 'axios'
 const router = express.Router()
 // app get question
 router.get("/:id?", async (req, res) => {
@@ -13,13 +13,14 @@ router.get("/:id?", async (req, res) => {
   router.post("/", async (req, res) => {
     try {
         const response = await axios.post('http://localhost:8080/question', {
-            user_id: req.params.user_id,
-            title: req.body.email,
+            user_id:req.body.user_id,
+            title: req.body.title,
             description:req.body.description
         })
         res.send(response.data)
     } catch (err) {
         res.send({ err })
+        console.log(err)
 
     }
 });
--- server/routes/verify.js
+++ server/routes/verify.js
@@ -1,13 +1,13 @@
 import express from 'express';
-import isAuthed from '../middleware.js';
+import isAuthed from '../isAuthed.js';
 const router = express.Router()
 router.get("/", async (req, res)=>{
     const verify = await isAuthed(req)
     res.json({
         verify:verify,
-        user_name:req.token.user_name,
-        email:req.token.email,
-        id: req.token.id
+        user_name:req.token?.user_name,
+        email:req.token?.email,
+        id: req.token?.id
     
     })
 })
